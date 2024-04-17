5b8e960be7dd19bc974fc87b72555452637aeaae
christinenl <christinenl@student.telkomuniversity.ac.id>
2024-04-17 18:26:54 +0700

    Update daftarpedagang.blade.php

--- resources/views/daftarpedagang.blade.php
+++ resources/views/daftarpedagang.blade.php
@@ -42,7 +42,7 @@
                   <span class="availability-status online"></span>
                 </div>
                 <div class="nav-profile-text">
-                  <p class="mb-1 text-black">Christine N</p>
+                  <p class="mb-1 text-black">Christine N.L</p>
                 </div>
               </a>
             </li>
@@ -71,7 +71,7 @@
                   <!--change to offline or busy as needed-->
                 </div>
                 <div class="nav-profile-text d-flex flex-column">
-                  <span class="font-weight-bold mb-2">Christine N</span>
+                  <span class="font-weight-bold mb-2">Christine N.L</span>
                   <span class="text-secondary text-small">Pembeli</span>
                 </div>
                 <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
