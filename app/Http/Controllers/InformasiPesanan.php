621fb4ff72e31b985a1809360da3c0bfb3823189
christinenl <christinenl@student.telkomuniversity.ac.id>
2024-05-21 20:12:01 +0700

    JG-1

--- app/Http/Controllers/InformasiPesanan.php
+++ app/Http/Controllers/InformasiPesanan.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class InformasiPesanan extends Controller
 {
-    //
+    public function index(){
+        $informasipesanan = InformasiPesanan::all();
+        return view(('informasipesanan'), compact('informasipesanan'));
+    }
 }
