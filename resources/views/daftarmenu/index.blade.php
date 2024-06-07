2ad9381758dc91a0337ceaef719da2a6b2fb065b
christinenl <christinenl@student.telkomuniversity.ac.id>
2024-06-08 00:17:45 +0700

    Update index.blade.php

--- resources/views/daftarmenu/index.blade.php
+++ resources/views/daftarmenu/index.blade.php
@@ -66,16 +66,34 @@
                   <!--change to offline or busy as needed-->
                 </div>
                 <div class="nav-profile-text d-flex flex-column">
-                  <span class="font-weight-bold mb-2">David Grey. H</span>
-                  <span class="text-secondary text-small">Project Manager</span>
+                  <span class="font-weight-bold mb-2">David</span>
+                  <span class="text-secondary text-small">Pedagang</span>
                 </div>
                 <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="daftarmenu">
                 <span class="menu-title">Daftar Menu</span>
                 <i class="mdi mdi-home menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="profile">
+                <span class="menu-title">Profile</span>
+                <i class="mdi mdi-contacts menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="riwayatpembelian">
+                <span class="menu-title">Riwayat Pembelian</span>
+                <i class="mdi mdi-format-list-bulleted menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pengaturan">
+                <span class="menu-title">Pengaturan</span>
+                <i class="mdi mdi-settings menu-icon"></i>
               </a>
             </li>
             <li>
