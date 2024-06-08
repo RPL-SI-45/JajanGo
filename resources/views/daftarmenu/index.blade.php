11b86b304adb37da01e8135a6cf5214fa9987525
christinenl <christinenl@student.telkomuniversity.ac.id>
2024-06-08 09:40:31 +0700

    Automatic Testing Profil Pedagang

--- resources/views/daftarmenu/index.blade.php
+++ resources/views/daftarmenu/index.blade.php
@@ -79,7 +79,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="{id}">
+              <a class="nav-link" href="1">
                 <span class="menu-title">Profile</span>
                 <i class="mdi mdi-contacts menu-icon"></i>
               </a>
